3868bda519998425cdd4b7466a2db281f65fd140
yavindu <yavindu@gmail.com>
2024-10-05 22:24:04 +0800

    add configs to yaml

--- src/database/db.py
+++ src/database/db.py
@@ -3,15 +3,20 @@ import time
 from beanie import init_beanie
 from motor.motor_asyncio import AsyncIOMotorClient
 
+from config.config import config as conf
 from src.document.documents import Conversation, Query
 
 
 async def init_db():
+    username = conf.config['mongodb']['username']
+    password = conf.config['mongodb']['password']
+    db_name = conf.config['mongodb']['db_name']
+
     # Create Motor client
-    client = AsyncIOMotorClient("mongodb://root:1234@localhost:27017")
+    client = AsyncIOMotorClient(f"mongodb://{username}:{password}@localhost:27017")
 
     # Init beanie with the Product document class
-    await init_beanie(database=client["gov_tech"], document_models=[Conversation, Query])
+    await init_beanie(database=client[f"{db_name}"], document_models=[Conversation, Query])
 
     conversion = Conversation(context="test", initiated_by="user", initiated_at=int(time.time()))
     await conversion.save()
