15ec9079a2659c2b781bada4ba4982969dc20bb3
yavindu <yavindu@gmail.com>
2024-10-05 22:36:41 +0800

    add configs

--- config/config.py
+++ config/config.py
@@ -5,9 +5,9 @@ import yaml
 
 class Config:
 
-    #load the config file
+    # load the config file
     def __init__(self, config_file):
-        #load yaml file
+        # load yaml file
         with open(config_file, 'r') as stream:
             try:
                 self.config = yaml.safe_load(stream)
--- src/database/db.py
+++ src/database/db.py
@@ -10,7 +10,7 @@ from src.document.documents import Conversation, Query
 async def init_db():
     username = conf.config['mongodb']['username']
     password = conf.config['mongodb']['password']
-    db_name = conf.config['mongodb']['db_name']
+    db_name = conf.config['mongodb']['name']
 
     # Create Motor client
     client = AsyncIOMotorClient(f"mongodb://{username}:{password}@localhost:27017")
